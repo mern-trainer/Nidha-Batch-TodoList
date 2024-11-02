b7caa80667341e66352628c773618c41930d068e
VISHNU M K <mail.vishnumk@gmail.com>
2024-11-02 13:30:17 +0530

    update in main.jsx - removed unused components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { Fragment, StrictMode } from 'react'
 import "./index.css"
 import { Toaster } from 'react-hot-toast';
 import { CounterProvider } from './Providers/CounterProvider.jsx';
